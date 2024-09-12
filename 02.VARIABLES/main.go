acb24e872748bb1f4fbc433471ee9a31a4674c14
Kishore-kumar-07 <bkdon54@gmail.com>
2024-09-12 22:07:07 +0530

    added 01 and 02

---
+++ 02.VARIABLES/main.go
@@ -0,0 +1,45 @@
+package main 
+
+import "fmt"
+var Public = "kishoooooroereeoreiuroe"
+
+const cnst = 02455
+
+func main() {
+	var s string = "Kishore"
+	fmt.Println(s)
+	fmt.Printf("Type %T \n",s)
+
+	var u uint8 = 255
+	fmt.Println(u)
+	fmt.Printf("Type %T \n",u)
+
+	var f32 = 23.232525644511456656
+	fmt.Println(f32)
+	fmt.Printf("Type %T \n",f32)
+
+	var i int = 256
+	fmt.Println(i)
+	fmt.Printf("Type %T \n",i)
+
+	var b bool = true 
+	fmt.Println(b)
+	fmt.Printf("Type %T \n",b)
+
+	var  varr = 2.002
+	fmt.Println(varr)
+	fmt.Printf("Type %T \n",varr)
+
+	valrous := "Kishore"
+	fmt.Println(valrous)
+	fmt.Printf("Type %T \n",valrous)
+
+
+	fmt.Println(Public)
+	fmt.Printf("Type %T \n",Public)
+
+    
+	fmt.Println(cnst)
+	fmt.Printf("Type %T \n",cnst)
+}
+
